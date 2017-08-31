d4fea3186db7791434e070f8b8ab148d7c0e3478
Natalia <natalia.kielbicka@o2.pl>
2017-08-31 15:13:46 +0200

    load data from flickr api

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 (function() {
     "use strict";
 
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1822a31c5cd782229698ed02217c7ea0&tags=flower&per_page=50&format=json&nojsoncallback=1";
 
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var answer = JSON.parse(this.responseText);
+            var data = answer.photos.photo;
+            //console.log(data)
+            loadData(data);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function loadData(arr) {
+        var out = "";
+        var i;
+        for (i = 0; i < arr.length; i++) {
+            out += '<img src="http://farm' + arr[i].farm + '.staticflickr.com/' + arr[i].server + '/' + arr[i].id + '_' + arr[i].secret + '.jpg">';
+        }
+        document.getElementById("response").innerHTML = out;
+    }
 
 })();
