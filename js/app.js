ca5a5cb0ca1aa9eab30ae2e1b5881fd9e978b74b
Natalia <natalia.kielbicka@o2.pl>
2017-08-31 17:55:28 +0200

    add openPhoto function and make some styling changes

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@
         if (this.readyState == 4 && this.status == 200) {
             var answer = JSON.parse(this.responseText);
             var data = answer.photos.photo;
-            //console.log(data)
             loadData(data);
         }
     };
@@ -18,27 +17,27 @@
     function loadData(arr) {
         var out = "";
         out += "<ul>";
-        for (var i = 0; i < arr.length; i++) {
-
-            // var aa = 'https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=1822a31c5cd782229698ed02217c7ea0&photo_id=' + arr[i].id;
-            // console.log(aa)
-            out += '<li><a href="#"><img src="http://farm' + arr[i].farm + '.staticflickr.com/' + arr[i].server + '/' + arr[i].id + '_' + arr[i].secret + '.jpg" class="photo"></a></li>';
+        for (var i = 0, arrLen = arr.length; i < arrLen; i++) {
+            out += '<li><a href="#"><img src="http://farm' + arr[i].farm + '.staticflickr.com/' + arr[i].server + '/' + arr[i].id + '_' + arr[i].secret + '.jpg" class="photo"><span class="close">x</span></a></li>';
         }
         out += "</ul>";
         document.getElementById("response").innerHTML = out;
+        openPhoto();
+    }
+
+    function openPhoto() {
+        var img = document.getElementsByTagName("img");
+        var close = document.getElementsByClassName("close");
+        for (var i = 0, imgLen = img.length; i < imgLen; i++) {
+            img[i].addEventListener("click", function() {
+                var li = this.parentNode.parentNode;
+                li.classList.add("active");
+            });
+            close[i].addEventListener("click", function() {
+                var li = this.parentNode.parentNode;
+                li.classList.remove("active");
+            });
+        }
     }
 
 })();
-// var request = new XMLHttpRequest();
-//     request.open("GET", 'https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=1822a31c5cd782229698ed02217c7ea0&photo_id=' + arr[i].id, false);
-//     request.send();
-//     var xml = request.responseXML;
-//     var users = xml.getElementsByTagName("photo");
-//     console.log(users)
-//     for (var j = 0; j < users.length; j++) {
-//         var user = users[j];
-//         var names = user.getElementsByTagName("exif");
-//         for (var k = 0; k < names.length; k++) {
-//             console.log(names[j].childNodes[0].nodeValue);
-//         }
-//     }
